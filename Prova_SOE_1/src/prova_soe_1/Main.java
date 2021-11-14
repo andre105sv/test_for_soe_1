97dc8cab9dde2f09fa158077fea30ec8df867b4f
andre105sv <a.landi88@studenti.unisa.it>
2021-11-14 20:01:57 +0100

    update con "good morning"

--- Prova_SOE_1/src/prova_soe_1/Main.java
+++ Prova_SOE_1/src/prova_soe_1/Main.java
@@ -10,6 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        hello("Good morning");
         hello("world");
         hello("universe");
     }
