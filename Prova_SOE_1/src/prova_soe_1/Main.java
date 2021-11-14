08e19343b78de42dd93206e76464b36775c0ab31
andre105sv <a.landi88@studenti.unisa.it>
2021-11-14 18:23:40 +0100

    Primo commit

---
+++ Prova_SOE_1/src/prova_soe_1/Main.java
@@ -0,0 +1,17 @@
+
+package prova_soe_1;
+
+/**
+ *
+ * @author splas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+    
+}
